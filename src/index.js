759ac04e1a1ad8f6c9375658bf923ce11be92a07
Luke Preston <lukep@ceh.ac.uk>
2018-02-07 08:08:45 +0000

    using components

--- src/index.js
+++ src/index.js
@@ -2,9 +2,8 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './app'
 import registerServiceWorker from './registerServiceWorker'
-import 'normalize.css'
-import 'bulma/css/bulma.css'
 import 'font-awesome/css/font-awesome.css'
+import '@blank-string/components'
 
 ReactDOM.render(<App />, document.getElementById('root'))
 registerServiceWorker()
